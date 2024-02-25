d0ace87fb61042ecc96a4119af5aa26cd56fea84
Shamik Guha Ray <shguhara@gmail.com>
2024-02-25 20:40:39 +0800

    re-deploy

--- terraform-iac-nginx/subnets.tf
+++ terraform-iac-nginx/subnets.tf
@@ -1,10 +1,11 @@
 resource "aws_subnet" "public_subnet" {
   count             = length(var.availability_zones)
   vpc_id            = data.aws_vpc.default.id
-  cidr_block        = element(var.subnet_cidr, count.index)
-  availability_zone = element(var.availability_zones, count.index)
+  cidr_block        = var.subnet_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
     Name = "Public_Subnet_${count.index + 1}"
   }
 }
+
